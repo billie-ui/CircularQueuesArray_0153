04f27f6c125af7e172ac417f110ac2c26c03079c
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-05-29 16:34:02 +0700

    Menambahkan menu display, exit, dan validasi pilihan user

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -148,3 +148,21 @@ int main()
                 q.remove();
                 break;
             }
+            
+            case '3':
+            {
+                q.display();
+                break;
+            }
+
+            case '4':
+            {
+                return 0;
+            }
+
+            default:
+            {
+                cout << "Invalid option!" << endl;
+            }
+            }
+        }
