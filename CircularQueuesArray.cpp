eb56f870d7ad8e1aaddede29547c519bdcd11fd6
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-05-29 16:38:52 +0700

    .

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -129,7 +129,7 @@ int main()
             cout << "Menu" << endl;
             cout << "1. Implement insert operation" << endl;
             cout << "2. Implement Delete Operation" << endl;
-            cout << "3. Display values " << endl;
+            cout << "3. Display values. " << endl;
             cout << "4. Exit " << endl;
             cout << "Enter your choice (1-4): ";
             cin >> ch;
