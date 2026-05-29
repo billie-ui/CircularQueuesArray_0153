5552b8237168381cfee5b23eb722d720efcef3b7
Billie Fauzan <kuliahkuliahbillie@gmail.com>
2026-05-29 15:51:02 +0700

    Membuat class Queue dengan atribut FRONT, REAR, dan array queue statis

--- CircularQueuesArray.cpp
+++ CircularQueuesArray.cpp
@@ -1,2 +1,17 @@
 #include <iostream>
 using namespace std;
+
+class Queues
+{
+private:
+    static const int max = 3;
+    int FRONT, REAR;
+    int queue_array[max];
+
+public:
+    Queue()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
+}
